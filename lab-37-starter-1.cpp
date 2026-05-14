23fb965bb018b53fe41e6d7f6c358f3651874170
gillianrhett <gillianrhett@gmail.com>
2026-05-14 14:55:13 -0700

    got it working with strings

--- lab-37-starter-1.cpp
+++ lab-37-starter-1.cpp
@@ -7,9 +7,10 @@ using namespace std;
 
 int gen_hash_index(string);
 list<char> convert_s_to_clist(string);
+bool search_map(int, map<int, string>&);
 
-const string FILENAME = "lab-37-data-3.txt";
-//const string FILENAME = "testdata.txt";
+//const string FILENAME = "lab-37-data-3.txt";
+const string FILENAME = "testdata.txt";
 
 int main() {
 
@@ -49,8 +50,8 @@ int main() {
 //    values that are std::lists
 
 //Create the std::map named hash_table. The key in the map is an int, the hash index. The value in a map is a std::list, which will contain the 12-character codes from the file that all map to that hash index.
-    map<int, list<char>> hash_table;
-    hash_table[0] = {'a','b'}; // testing
+    map<int, string> hash_table; // I'll get it to work with a string first, then convert to a list of chars
+    hash_table[0] = {"abc"}; // testing
     cout << hash_table.count(0) << endl; // testing
     string str_in; // for getting each line from the file
 //When codes are read from the file, send the code to your function. Receive its hash index that's returned from the function. Input that pair into the map (the hash index and the code string). Remember that the code string is going into a std::list, so use the appropriate method to manipulate that std::list.
@@ -59,15 +60,18 @@ int main() {
         int temp_key = gen_hash_index(str_in);
 
         // check whether this key is already taken
-        while(hash_table.count(temp_key))
+        while(search_map(temp_key, hash_table))
             temp_key += 1; // get a key that isn't already taken, via simple linear probe
-        list<char> char_list = convert_s_to_clist(str_in);
-        // store the string's chars in the list
         
+        //list<char> char_list = convert_s_to_clist(str_in);
+        // store the string's chars in the list
+        hash_table[temp_key] = {str_in};
     }
 
 //Display just the first 100 map entries to the console to test your data structure. Remember how to access map elements with .first and .second as necessary.
-
+    int max = 100;
+    for (auto pair : hash_table)
+        cout << pair.second << endl;
 //Your final submission is Milestone 3.
 
     return 0;
@@ -88,6 +92,11 @@ list<char> convert_s_to_clist(string str_in) {
     for(char c : str_in)
         c_list.push_back(c);
     return c_list;
+}
+
+bool search_map(int search_key, map<int, string>& map_to_search) {
+    auto it = map_to_search.find(search_key);
+    return it != map_to_search.end();
 }
 
 /* 
