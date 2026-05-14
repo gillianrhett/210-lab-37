7598e3214b761d19dc16b9f12e7b9c57db03c987
gillianrhett <gillianrhett@gmail.com>
2026-05-14 15:28:05 -0700

    I was not almost done. Linear is very slow with large data set.

--- lab-37-starter-1.cpp
+++ lab-37-starter-1.cpp
@@ -7,17 +7,11 @@ using namespace std;
 
 int gen_hash_index(string);
 
-//const string FILENAME = "lab-37-data-3.txt";
-const string FILENAME = "testdata.txt";
+const string FILENAME = "lab-37-data-3.txt";
+//const string FILENAME = "testdata.txt";
+const int MAX_SIZE = 100185; // the number of items we need to store
 
 int main() {
-
-    /* Milestone 1
-    string word = "Hello"; // 72 + 101 + 108 + 108 + 111 = 500
-    cout << sum_ascii(word) << endl;
-    word = "world!"; // 119 + 111 + 114 + 108 + 100 + 33 = 585
-    cout << sum_ascii(word) << endl;
-    */
 
     ifstream inFile; // file object to get the items from
     // try to open the file
@@ -30,34 +24,22 @@ int main() {
         cout << "Error: " << e.what();
         return -1;
     }
-    /* Milestone 2
-    int grand_total = 0;
-    // read each string from the file and add its int value to the grand total
+
+    map<int, list<char>> hash_table;
     string str_in; // for getting each line from the file
-    while(inFile >> str_in) {
-        grand_total += sum_ascii(str_in);
-    }
 
-    cout << grand_total << endl; // it should be 69893419
-    // I got 69894129, difference is 710. It's getting the last line twice
-    // fixed by putting inFile >> str_in in the while condition instead of !eof
-    */
-
-//We'll now create the hash table data structure. It'll be a map comprised of an int and a list as key-value pairs:
-//    keys that are integers
-//    values that are std::lists
-
-//Create the std::map named hash_table. The key in the map is an int, the hash index. The value in a map is a std::list, which will contain the 12-character codes from the file that all map to that hash index.
-    map<int, list<char>> hash_table; // I'll get it to work with a string first, then convert to a list of chars
-    string str_in; // for getting each line from the file
-//When codes are read from the file, send the code to your function. Receive its hash index that's returned from the function. Input that pair into the map (the hash index and the code string). Remember that the code string is going into a std::list, so use the appropriate method to manipulate that std::list.
+    // read each string from the file, compute a suitable index, and insert it in the map
     while(inFile >> str_in) {
         // create the hash
         int temp_key = gen_hash_index(str_in);
 
         // check whether this key is already taken
-        while(hash_table.count(temp_key) > 0)
-            temp_key += 1; // get a key that isn't already taken, via simple linear probe
+        int x = 0; // for quadratic probe
+        while(hash_table.count(temp_key) > 0) {
+            temp_key = ; // get a key that isn't already taken
+            if (temp_key > MAX_SIZE)
+                temp_key = 0; // wrap back around to 0
+        }
 
         // store the string's chars in the list
         for (char c : str_in)
